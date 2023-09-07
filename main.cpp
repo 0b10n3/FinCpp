8bcc62df9b5cc450cbbd14bd4bba4513dcb530bc
Ob1On3 <silvasmath@gmail.com>
2023-09-06 22:24:23 -0300

    Integer division.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
+    // Integer data types
+    // int
+    int numberOfStudents = 10;
+    int numberOfTeachers = 3;
+
+    std::cout << "Number of students: " << numberOfStudents << std::endl;
+    std::cout << "Number of teachers: " << numberOfTeachers << std::endl;
+    std::cout << "Teachers to students ratio: " << numberOfTeachers / numberOfStudents << std::endl;
+
     std::cout << "Hello World!" << std::endl;
 
     return 0;
