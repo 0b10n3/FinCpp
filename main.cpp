f1a106e2b17f5b6eab30927fdcbd041b1e8d95b0
Ob1On3 <silvasmath@gmail.com>
2023-09-06 22:25:54 -0300

    Double fixes the precision on division.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ int main()
     // Integer data types
     // int
     int numberOfStudents = 10;
-    int numberOfTeachers = 3;
+    double numberOfTeachers = 3;
 
     std::cout << "Number of students: " << numberOfStudents << std::endl;
     std::cout << "Number of teachers: " << numberOfTeachers << std::endl;
-    std::cout << "Teachers to students ratio: " << numberOfTeachers / numberOfStudents << std::endl;
+
+    double ratio = numberOfTeachers / numberOfStudents;
+
+    std::cout << "Teachers to students ratio: " << ratio << std::endl;
 
     std::cout << "Hello World!" << std::endl;
 
